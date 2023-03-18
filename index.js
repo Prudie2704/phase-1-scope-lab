474de4b1e7b44491fce075c22aea672611476d04
prudence <prudence.jerono@student.moringaschool.com>
2023-03-18 17:09:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+function name() {
+    return customerName;
+}
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+let bestCustomer = 'not bob';
+    
+function overwriteBestCustomer(newFavourite){
+    return bestCustomer = newFavourite
+}
+function leastFavouriteCustomer(changeLeastFavouriteCustomer){
+    return changeLeastFavouriteCustomer = 'maybe bob';
+}
